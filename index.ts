aefed562b751bccfe4eebdfef5cb01b32a507165
Abel Garcia Dorta <abelgarcia@acidtango.com>
2021-05-11 14:49:21 +0100

    feat: support for private repositories
    * add auth command line argument
    * fix a problem when fetching workflow runs

--- index.ts
+++ index.ts
@@ -9,16 +9,22 @@ const optionDefinitions: OptionDefinition[] = [
     type: String,
     defaultValue: "rest.js",
   },
+  { name: "authentication", alias: "a", type: String },
 ];
 
+const ROWS_PER_PAGE = 100;
 const options = commandLineArgs(optionDefinitions);
 
-const octokit = new Octokit();
+const octokit =
+  options.authentication === undefined
+    ? new Octokit()
+    : new Octokit({ auth: options.authentication });
+
 // list all public repos belonging to a given organization from commmand line arg
 octokit.rest.repos
   .listForOrg({
     org: options.organization,
-    type: "public",
+    type: options.authentication === undefined ? "public" : "private",
   })
   .then(({ data }) => {
     data
@@ -32,26 +38,39 @@ octokit.rest.repos
             owner: repository.owner?.login as string,
           })
           .then(({ data }) => {
-            data.workflow_runs.map((workflow) =>
-              // for all workflows previously retrieved, get all jobs
+            const pages = Math.ceil(data.total_count / ROWS_PER_PAGE);
+            const pagesArr = new Array(pages).fill(null).map((_, i) => i + 1);
+            // get all pages in parallel
+            pagesArr.map((p) =>
               octokit.rest.actions
-                .listJobsForWorkflowRun({
-                  owner: repository.owner?.login as string,
+                .listWorkflowRunsForRepo({
                   repo: repository.name,
-                  run_id: workflow.id,
+                  owner: repository.owner?.login as string,
+                  page: p,
+                  per_page: ROWS_PER_PAGE,
                 })
                 .then(({ data }) =>
-                  data.jobs.map((job) =>
-                    // for all jobs previously retrieved, print a csv line to stdout
-                    // after that you can paste it into a spreadsheet
-                    // TODO: use a CSV library
-                    console.log(
-                      job.id +
-                        ", " +
-                        new Date(job.started_at).getTime() +
-                        ", " +
-                        new Date(job.completed_at).getTime()
-                    )
+                  data.workflow_runs.map((workflow) =>
+                    octokit.rest.actions
+                      .listJobsForWorkflowRun({
+                        owner: repository.owner?.login as string,
+                        repo: repository.name,
+                        run_id: workflow.id,
+                      })
+                      .then(({ data }) =>
+                        data.jobs.map((job) =>
+                          // for all jobs previously retrieved, print a csv line to stdout
+                          // after that you can paste it into a spreadsheet
+                          // TODO: use a CSV library
+                          console.log(
+                            job.id +
+                              ", " +
+                              new Date(job.started_at).getTime() +
+                              ", " +
+                              new Date(job.completed_at).getTime()
+                          )
+                        )
+                      )
                   )
                 )
             );
